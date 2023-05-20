f6b4cd2853aad57afc915f7583e9ca0c89919efb
SickMarco <marcobo97@hotmail.it>
2023-05-20 15:58:59 +0200

    sedV2

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -8,30 +8,31 @@ int main(int ac, char **av)
 	{
 		std::string filename = av[1];
 		std::string s1 = av[2];
-		std::string s2 = av[3]; 
-		std::ifstream file;
-		
+		std::string s2 = av[3];
+
+		std::ifstream file;	
 		file.open(filename.c_str());
-		if (!file.is_open()) { std::cout << "Error: File not found." << std::endl; return 1; }
+		if (!file.is_open()) { std::cout << "\033[31mError: File not found." << std::endl; return 1; }
 		else
 		{
-			std::string line;
 			std::ofstream file2(filename.append(".replace").c_str());
+			if (!file2) { std::cout << "\033[31mError: File creation failed." << std::endl; return 1; }
+			std::string line;
 			while (std::getline(file, line))
 			{
 				size_t pos = 0;
 				while ((pos = line.find(s1, pos)) != std::string::npos)
 				{
-					line.replace(pos, s2.length(), s2);
+					line.replace(pos, s1.length(), s2);
 					pos += s2.length();
 					file2 << line << std::endl;
 				}
 			}
 			file.close();
 			file2.close();
+			return 0;
 		}
 	}
-	else
-	{ std::cout << "Error: Wrong arguments. Insert <filename> <s1> <s2>" << std::endl; return 1; };
-	return 0;
+	else std::cout << "\033[31mError: Wrong arguments. Insert <filename> <s1> <s2>" << std::endl;
+	return 1;
 }
