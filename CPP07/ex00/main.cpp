eec3fed3c6f276d607779c6ffc4cabfacd829ca1
Marco Bozzi <marcobo97@hotmail.it>
2023-06-13 18:49:38 +0200

    Template

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbozzi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 17:59:47 by mbozzi            #+#    #+#             */
+/*   Updated: 2023/06/13 18:25:11 by mbozzi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) {
+
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbozzi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 18:00:57 by mbozzi            #+#    #+#             */
+/*   Updated: 2023/06/13 18:25:15 by mbozzi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template<typename T>
+void swap(T& a, T& b){
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename T>
+T min(T& a, T& b){
+	return (a < b) ? a : b;
+}
+
+template<typename T>
+T max(T& a, T& b){
+	return (a > b) ? a : b;
+}
+
+#endif
